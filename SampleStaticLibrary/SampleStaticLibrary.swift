c384f922d6cd537022377f758f21fad1835b4fd4
mocchan <hcmnorgir0808@yahoo.co.jp>
2024-03-20 05:46:50 +0900

    initial commit

---
+++ SampleStaticLibrary/SampleStaticLibrary.swift
@@ -0,0 +1,14 @@
+//
+//  SampleStaticLibrary.swift
+//  SampleStaticLibrary
+//
+//  Created by 岩本康孝 on 2024/03/20.
+//
+
+public class StaticLibrary {
+    public init() {}
+
+    public func displayFunction() {
+        print(#function)
+    }
+}
